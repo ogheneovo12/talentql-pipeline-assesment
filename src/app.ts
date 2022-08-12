6c285043ea67cf8a710cbad0951adcc536ea2a4d
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2022-08-12 05:07:51 +0100

    finalized with assesment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,147 @@
+/** START DEFINE TYPES AND INTERFACES **/
+type PageData = Record<string, IUserData[]>[];
+interface IState {
+  currentPage: number;
+  pageData: PageData;
+  paging?: IPaging;
+}
 
-const startApp = async () => {
-    
+interface IUserData {
+  id: string;
+  row: number;
+  age: number;
+  gender: "male" | "female";
+}
+
+interface IPaging {
+  next: string;
+  previous: string;
+}
+
+interface IFetchResponse {
+  results: PageData;
+  paging?: IPaging;
+}
+
+/** END DEFINE TYPES AND INTERFACES **/
+/** DOM CONSTANTS  */
+
+const tbody = document.querySelector<HTMLElement>("#tbody"), //insert rows into the table body
+  prevBtn = document.querySelector<HTMLButtonElement>("#prev"), // button to navigate to previous page
+  nextBtn = document.querySelector<HTMLButtonElement>("#next"), // button to navigate to next page
+  pageIndicator = document.querySelector<HTMLElement>("#page_indicator"), //current page indicator
+  loadingUi = document.querySelector<HTMLElement>(".loader-overlay"); // div to indicate page is loading
+
+const BASE_URL = "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84"; //BASE DATA API URL
+
+const state: IState = {
+  currentPage: 1,
+  pageData: [],
 };
 
-document.addEventListener('DOMContentLoaded', startApp);
+const showLoadingUi = () => {
+  if (loadingUi) {
+    loadingUi.style.display = "flex";
+  }
+};
+
+const hideLoadingUi = () => {
+  if (loadingUi) {
+    loadingUi.style.display = "none";
+  }
+};
+
+const handleNext = () => {
+  //CHECK IF PAGE N+1 EXIST IN RECORD
+  if (getPageData(state.currentPage + 1)) {
+    //update current page and then redraw table
+    state.currentPage = state.currentPage + 1;
+    insertIntoDom();
+  } else {
+    //FETCH PAGE FROM SERVER
+    fetchPageData(state.currentPage + 1);
+  }
+};
+
+const handlePrev = () => {
+  //check if we are at start index and return
+  if (state.currentPage <= 1) return;
+
+  //CHECK IF PAGE N-1 EXIST IN RECORD
+  if (getPageData(state.currentPage - 1)) {
+    //update current page and then redraw table
+    state.currentPage = state.currentPage - 1;
+    insertIntoDom();
+  } else {
+    //FETCH PAGE FROM SERVER
+    fetchPageData(state.currentPage - 1);
+  }
+};
+
+const createTd = (textContent: string) => {
+  const Td = document.createElement("td");
+  Td.textContent = textContent;
+  return Td;
+};
+
+const createTr = (rowData: IUserData, rowIndex: number) => {
+  const Tr = document.createElement("tr");
+  Tr.dataset["entryid"] = rowData.id;
+  Tr.replaceChildren(
+    createTd(`${rowIndex}`),
+    createTd(rowData.gender),
+    createTd(`${rowData.age}`)
+  );
+  return Tr;
+};
+
+const getPageData = (N: number) => state.pageData[0][N]?.map(createTr);
+
+const insertIntoDom = () => {
+  const tableRows = getPageData(state.currentPage);
+  tbody?.replaceChildren(...tableRows);
+  if (pageIndicator) {
+    pageIndicator.textContent = `${state.currentPage}`;
+  }
+  shouldDisablePrev();
+};
+const shouldDisablePrev = () => {
+  if (prevBtn) {
+    if (state.currentPage > 1) {
+      prevBtn.disabled = false;
+    } else {
+      prevBtn.disabled = true;
+    }
+  }
+};
+
+const setState = (newState: IState) => {
+  state.currentPage = newState?.currentPage;
+  state.pageData = newState?.pageData;
+  state.paging = newState?.paging;
+};
+
+const fetchPageData = (page = 1) => {
+  showLoadingUi();
+  fetch(`${BASE_URL}&page=${page}`)
+    .then((response) => response.json())
+    .then((data: IFetchResponse) => {
+      setState({
+        pageData: data.results,
+        currentPage: page,
+        paging: data?.paging,
+      });
+      insertIntoDom();
+      hideLoadingUi();
+    });
+};
+
+prevBtn?.addEventListener("click", handlePrev);
+nextBtn?.addEventListener("click", handleNext);
+
+const startApp = async () => {
+  shouldDisablePrev();
+  fetchPageData();
+};
+
+document.addEventListener("DOMContentLoaded", startApp);
